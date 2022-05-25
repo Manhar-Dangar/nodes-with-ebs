93b5f61a2284f2589915269f6bcf02250468e006
ManharDangar <manhar.dangar.sa@gmail.com>
2022-05-25 14:05:51 +0530

    add new one

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("VERSION 1.9");
+  res.send("VERSION 2");
 });
-app.get("/new", function (req, res) {
-  res.send("New also worked");
+app.get("/blog", function (req, res) {
+  res.send("Welcome to blog");
 });
 
 app.listen(process.env.PORT || 5000);
